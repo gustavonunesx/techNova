3db71b9fa9b1070de8d017b9f9f5229e31f2335b
V1ct0rhugo22 <victorhero53@gmail.com>
2025-06-04 17:16:41 -0300

    Web config

---
+++ hackaton/src/main/java/com/senai/hackaton/WebConfig/WebConfig.java
@@ -0,0 +1,18 @@
+package com.senai.hackaton.WebConfig;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+public class WebConfig implements WebMvcConfigurer {
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
+        // Permite qualquer origem com credenciais
+        registry.addMapping("/**")
+                .allowedOriginPatterns("*")
+                .allowedMethods("*")
+                .allowedHeaders("*")
+                .maxAge(3600);
+    }
+}
